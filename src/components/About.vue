670d711a93cc245a182c02ac7a0f2887c1cef4c2
CodingEye <Huai0125@163.com>
2017-03-17 21:29:36 +0800

    Initial Create

---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    This is About
+    <MenuBar></MenuBar>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import MenuBar from './MenuBar.vue'
+
+  export default{
+    components: {
+      MenuBar
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
