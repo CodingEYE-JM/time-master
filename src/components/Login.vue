2097c8b6fff405fe6d720896b4dd6b6ec580b2d4
CodingEye <Huai0125@163.com>
2017-03-17 21:33:42 +0800

    Finish Basic Structure

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,7 +10,9 @@
       <input type="password">
     </div>
     <div>
-      <button>Submit</button>
+      <router-link to="/report">
+        <button>Submit</button>
+      </router-link>
     </div>
   </div>
 </template>
@@ -27,22 +29,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="stylus" rel="stylesheet/stylus" scoped>
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
